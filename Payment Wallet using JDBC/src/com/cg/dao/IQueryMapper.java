88d52690a2f78bd00e80481a70fe9b0eca7e6479
beingshuchi <shuchitamail@gmail.com>
2018-07-28 17:01:24 +0530

    Update IQueryMapper.java

--- Payment Wallet using JDBC/src/com/cg/dao/IQueryMapper.java
+++ Payment Wallet using JDBC/src/com/cg/dao/IQueryMapper.java
@@ -3,6 +3,6 @@ package com.cg.dao;
 public interface IQueryMapper {
 	public String Insert ="INSERT INTO Account( name,email,mobileno,balance) VALUES(?, ?, ?, ?)";
 	public String getLastMobNo="SELECT MAX(mobileno) FROM Account";
-	public String getAcc="SELECT * FROM Account WHERE ?";
-	public String getBal="SELECT balance FROM Account WHERE ?";
+	public String getAcc="SELECT * FROM Account WHERE mobileno= ?";
+	public String getBal="SELECT balance FROM Account WHERE mobileno=?";
 }
